6835dc9673e8b2a41919120ff197f98a17724ad4
rlmcneary2 <rich@bitovi.com>
2022-04-27 14:55:46 -0400

    Find an embedded issue number.

--- action-qa-words/index.js
+++ action-qa-words/index.js
@@ -1,11 +1,31 @@
 import core from "@actions/core";
 import { context } from "@actions/github";
 
-try {
-  // core.debug(JSON.stringify(context, null, 2));
-  core.info("context=");
-  core.info(JSON.stringify(context, null, 2));
-  core.setOutput("issueId", "1");
-} catch (err) {
-  core.setFailed(err.message);
+function main() {
+  try {
+    // core.debug(JSON.stringify(context, null, 2));
+    core.info("context=");
+    core.info(JSON.stringify(context, null, 2));
+
+    const {
+      payload: {
+        pull_request: { body },
+      },
+    } = context;
+    core.info(`body='${body}'`);
+
+    const matches = /#[\s]?([1-9]+)/.exec(body);
+    core.info(`matches='${JSON.stringify(matches)}'`);
+
+    if (!matches.length) {
+      core.setOutput("issueId", "");
+      return;
+    }
+
+    core.setOutput("issueId", matches[0]);
+  } catch (err) {
+    core.setFailed(err.message);
+  }
 }
+
+main();
