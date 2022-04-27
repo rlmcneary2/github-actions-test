f70de8039c934cf67dbbeecf880a5a64f7d48b83
rlmcneary2 <rich@bitovi.com>
2022-04-27 18:23:48 -0400

    Add the qa-pipeline action.

---
+++ action-qa-pipeline/index.js
@@ -0,0 +1,19 @@
+import core from "@actions/core";
+import { context } from "@actions/github";
+
+function main() {
+  try {
+    // core.info(`context=\n${JSON.stringify(context, null, 2)}`);
+
+    const issueIds = core.getInput("issueIds");
+    const ids = issueIds.split(",");
+
+    core.info(`ids=${JSON.stringify(ids)}`);
+
+    core.setOutput("issueIds", issueIds.join(","));
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+}
+
+main();
