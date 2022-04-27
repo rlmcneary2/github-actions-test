0081656a65fb2c3658905b3723278d985a6d87e6
rlmcneary2 <rich@bitovi.com>
2022-04-27 12:22:50 -0400

    Add info message.

--- action-qa-words/index.js
+++ action-qa-words/index.js
@@ -2,7 +2,9 @@ import core from "@actions/core";
 import { context } from "@actions/github";
 
 try {
-  core.debug(JSON.stringify(context, null, 2));
+  // core.debug(JSON.stringify(context, null, 2));
+  core.info("context=");
+  core.info(JSON.stringify(context, null, 2));
   core.setOutput("issueId", "1");
 } catch (err) {
   core.setFailed(err.message);
