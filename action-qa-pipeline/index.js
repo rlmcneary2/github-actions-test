907e3bbfcd4c8f7d3eddb65840012c4fbd8a2665
rlmcneary2 <rich@bitovi.com>
2022-04-28 10:57:11 -0400

    Start work on zenhub fetch.

--- action-qa-pipeline/index.js
+++ action-qa-pipeline/index.js
@@ -1,7 +1,8 @@
 import core from "@actions/core";
 import { context } from "@actions/github";
+import fetch from "node-fetch";
 
-function main() {
+async function main() {
   try {
     // core.info(`context=\n${JSON.stringify(context, null, 2)}`);
 
@@ -9,9 +10,30 @@ function main() {
     core.info(`issueIds='${issueIds}'`);
 
     const ids = issueIds.split(",");
-    core.info(`ids=${JSON.stringify(ids)}`);
 
-    core.setOutput("issueIds", issueIds);
+    // If this is from a PR was the PR merged>
+    const merged = context.payload.pull_request?.merged;
+    core.info(`merged='${merged}'`);
+
+    if (!merged) {
+      return;
+    }
+
+    const repoId = context.payload.repository?.id;
+    const workspaceId = "TODO";
+
+    await Promise.all(ids.map(id => {
+      const url = new URL("https://https://api.zenhub.com");
+      url.pathname = `/p2/workspaces/${workspaceId}/repositories/${repoId}/issues/${id}/moves`;
+
+      const init = {
+        method: "POST"
+      };
+
+      const response = await fetch(url, init);
+      core.info(`response=\n${JSON.stringify(response, null, 2)}`);
+    }));
+
   } catch (err) {
     core.setFailed(err.message);
   }
