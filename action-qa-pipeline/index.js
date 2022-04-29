5d5d89fb8b4e11e0b10c0c4314f0d31548cfe5e3
rlmcneary2 <rich@bitovi.com>
2022-04-29 16:47:29 -0400

    Add missing async.

--- action-qa-pipeline/index.js
+++ action-qa-pipeline/index.js
@@ -23,25 +23,27 @@ async function main() {
     const engineeringWorkspaceId = "60997aff441f2f0011219bc1";
     const qaPipelineId = "Z2lkOi8vcmFwdG9yL1BpcGVsaW5lLzI2ODAzMTE";
 
-    await Promise.all(ids.map(id => {
-      const url = new URL("https://https://api.zenhub.com");
-      url.pathname = `/p2/workspaces/${engineeringWorkspaceId}/repositories/${repoId}/issues/${id}/moves`;
+    await Promise.all(
+      ids.map(async (id) => {
+        const url = new URL("https://https://api.zenhub.com");
+        url.pathname = `/p2/workspaces/${engineeringWorkspaceId}/repositories/${repoId}/issues/${id}/moves`;
 
-      const init = {
-        body: JSON.stringify({
-          pipeline_id: qaPipelineId,
-          position: "bottom"
-        }),
-        headers: {
-          "X-Authentication-Token": "85e7967f659573178f3d7cf6bdf6d51a3e723eb03447321d431c1d150fbd491d0eaeb00014ba73ed"
-        },
-        method: "POST"
-      };
+        const init = {
+          body: JSON.stringify({
+            pipeline_id: qaPipelineId,
+            position: "bottom",
+          }),
+          headers: {
+            "X-Authentication-Token":
+              "85e7967f659573178f3d7cf6bdf6d51a3e723eb03447321d431c1d150fbd491d0eaeb00014ba73ed",
+          },
+          method: "POST",
+        };
 
-      const response = await fetch(url, init);
-      core.info(`response[${id}]=\n${JSON.stringify(response, null, 2)}`);
-    }));
-
+        const response = await fetch(url, init);
+        core.info(`response[${id}]=\n${JSON.stringify(response, null, 2)}`);
+      })
+    );
   } catch (err) {
     core.setFailed(err.message);
   }
